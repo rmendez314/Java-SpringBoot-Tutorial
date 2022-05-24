4fb206d4513b9b01792ccc3fb7bf81749b089ae2
Ramiro Mendez <90067776+rmendez314@users.noreply.github.com>
2022-05-24 00:04:58 -0500

    successfully completed a PUT endpoint

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -2,7 +2,10 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -26,6 +29,40 @@ public class StudentService {
             throw new IllegalStateException("Student with email " + student.getEmail() + " already exists");
         } else {
             studentRepository.save(student);
+        }
+    }
+
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException("Student with id " + studentId + " does not exist");
+        } else {
+            studentRepository.deleteById(studentId);
+        }
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+        Student student = studentRepository.findById(studentId).orElseThrow(()
+                -> new IllegalStateException(
+                        "Student with id " + studentId + " does not exist"));
+        if(name != null &&
+            name.length() > 0 &&
+                    !Objects.equals(student.getName(), name)) {
+                student.setName(name);
+        }
+        if(email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional =
+                    studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()) {
+                throw new IllegalStateException("Student with email " + email + " already exists");
+            } else {
+                student.setEmail(email);
+            }
         }
     }
 }
